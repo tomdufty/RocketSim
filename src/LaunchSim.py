0499d8e1c1156207146c8a6735e49eaac121f2c0
tdufty <tom.dufty@hotmail.com>
2019-04-25 15:57:46 +1000

    atmosphere shuffled to allow calculation of lift

--- src/LaunchSim.py
+++ src/LaunchSim.py
@@ -1,49 +1,48 @@
 import math
 from src import AtmoSim
 from src import VehicleSim as VS
-import numpy as np
-import csv
-import matplotlib
-import PyQt5
 
 GRAVITY = -9.81
+
 
 class ControlScheme:
 
     def __init__(self):
         print('new control scheme')
         self.targetvariable = 'total acceleration' #vertical acceleartion, total acceleration,drag,lift,dyamic pressure
-        self.controlfactor = 'angle of attacl'  #''throttle' or 'rate of climb'
+        self.controlfactor = 'angle of attack'  #''throttle' or 'rate of climb'
         self.varyingfactor = 'time' #altitude, total velcocity, horizontal velocity
         self.controltable = []
         self.rateofclimb = 10 # angle of climb set and to be swept or used as rate of climb
-        self.targetaltitude =100000
+        self.targetaltitude = 100000
         self.targetxvelocity = 1000
         self.takeOffV = 300
         self.takeoffAng = 0
 
-    def addtarget(self,x,target):
-        targetentry = [x,target]
+    def addtarget(self, x, target):
+        targetentry = [x, target]
         self.controltable.append(targetentry)
-
 
 
 class Launch:
     timestep = 0.1
-    endtime =1000
+    endtime = 1000
 
     def __init__(self):
         print("new launch")
         self.fs = FlightState()
         self.log = []
+        print("loading atmosphere")
 
-    def setTakeoff(self,v,theta):
+    def setTakeoff(self, v, theta):
+
         print("setting take off parameters")
         TO = FlightState()
-        TO.vy = math.sin(math.radians(theta))*v
+        TO.vy = math.sin(math.radians(theta)) * v
         TO.vx = math.cos(math.radians(theta)) * v
         TO.aot = theta
         self.fs = TO
+
 
 class FlightState:
 
@@ -59,24 +58,7 @@ class FlightState:
         self.aot = 0
         self.wz = 0
         print("new state")
-
-    def setax(self,ax):
-        self.ax = ax
-
-    def setay(self,ay):
-        self.ay = ay
-
-    def setvx(self,vx):
-        self.vx = vx
-
-    def setvy(self,vy):
-        self.vy = vy
-
-    def setsx(self,sx):
-        self.sx = sx
-
-    def setsy(self,sy):
-        self.sy = sy
+        self.atmo = AtmoSim.load_atmosphere()
 
     def calc_centr_acc(self):
         print("calculating centrifugal accelaration")
@@ -94,31 +76,36 @@ class FlightState:
         # note thrust and angle of thrust constant for timestep - to be updated when thrust more acurate.
         #acceleration based on avearage mass taken as linear avearge of timestep
 
-        fuel0 = 1000 #self.vehicle.fueli
+        #calculate fuel reduction
+        fuel0 = self.vehicle.fueli
         self.vehicle.useFuel(self.vehicle.thrust, timestep)
         fuel1 = self.vehicle.fueli
-
         avmass = self.vehicle.mass + (fuel0+fuel1)/2
 
+        #calculate lift
+        sos = AtmoSim.get_atmo_value(self.atmo,self.sy,'sos')
+        print ("sos",sos)
+        M = self.vx/sos
+        #cl = self.vehicle.lookup_cl(M)
+        #print(cl)
+        #self.vehicle.calc_lift()
+
         #calcualte acceleartion for period
-        ax0 = self.ax
-        ay0 = self.ay
+
         self.ax = self.vehicle.thrust*math.cos(math.radians(self.aot))/(avmass)
-        print("ax =",self.vehicle.thrust*math.cos(math.radians(self.aot))/(avmass))
         self.ay = GRAVITY + self.vehicle.thrust * math.sin(math.radians(self.aot))/avmass
 
         # calculate velocity - velocity at end based on constant average acceleration to be corrected when thrust varied
         vx0 = self.vx
         vy0 = self.vy
-        vx1 = self.vx + (self.ax+ax0)/2*timestep
-        vy1 = self.vy + (self.ay+ay0)/2*timestep
+        vx1 = self.vx + self.ax*timestep
+        vy1 = self.vy + self.ay*timestep
         self.vx = vx1
         self.vy = vy1
 
         #calcualte displacement based on average velocity
         self.sx = self.sx + (vx0 + vx1)/2*timestep
         self.sy = self.sy + (vy0 + vy1)/2*timestep
-
 
 
 
